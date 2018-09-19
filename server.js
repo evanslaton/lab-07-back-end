2bd72e676262d331709871a6de5d7dc2b41ce3d0
Evan Slaton <evanslaton@gmail.com>
2018-09-19 10:24:56 -0700

    Created constructor function

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 // Retrieves information about the location sent by the user using Google's Map API
 app.get('/location', (request, response) => {
   // Checks for signs of life, will delete when finished
@@ -22,7 +24,7 @@ app.get('/location', (request, response) => {
     .then(result => {
       // Creates an object containing the data to return to the client
       const location = {
-        search_query: request.query.data, // Location the user is requesting data for
+        search_query: request.query.data, // The user's input
         formatted_query: result.body.results[0].formatted_address, // City, state and country of the user's input
         latitude: result.body.results[0].geometry.location.lat, // Latitude of the user's input
         longitude: result.body.results[0].geometry.location.lng // Longitude of the user's input
@@ -31,5 +33,29 @@ app.get('/location', (request, response) => {
     })
 })
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Creates Weather object
+function Weather(day) {
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+  this.forecast = day.summary;
+}
+
+// Creates Yelp object
+function Yelp(data) {
+  this.url = data.business[0].url;
+  this.name = data.business[0].name;
+  this.rating = data.business[0].rating;
+  this.price = data.business[0].price;
+  this.image_url = data.business[0].image_url;
+}
+
+// Ceeates The Movide DB object
+function TMDB(data) {
+  this.title = data.title;
+  this.released_on = data.release_date;
+  this.total_votes = data.vote_count;
+  this.average_votes = data.vote_average;
+  this.popularity = data.popularity;
+  this.image_url = `https://image.tmdb.org/t/p/w500${data.backdrop_path}`;
+  this. overview = data.overview;
+}
 
